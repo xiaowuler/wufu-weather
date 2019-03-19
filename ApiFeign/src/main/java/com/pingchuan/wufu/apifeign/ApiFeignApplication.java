02cb79b8d0f89b7d742cec5a2333b790fecfd5a6
小武 <xiaowuler@163.com>
2019-03-19 16:29:43 +0800

    3/19 16:28 接口添加swagger2依赖，自动生成接口文档

--- ApiFeign/src/main/java/com/pingchuan/wufu/apifeign/ApiFeignApplication.java
+++ ApiFeign/src/main/java/com/pingchuan/wufu/apifeign/ApiFeignApplication.java
@@ -1,9 +1,12 @@
 package com.pingchuan.wufu.apifeign;
 
+import com.pingchuan.wufu.apifeign.Configs.Swagger2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
+@Import(Swagger2Config.class)
 @SpringBootApplication
 @EnableFeignClients
 public class ApiFeignApplication {
--- ApiFeign/src/main/java/com/pingchuan/wufu/apifeign/Controller/UserController.java
+++ ApiFeign/src/main/java/com/pingchuan/wufu/apifeign/Controller/UserController.java
@@ -3,10 +3,13 @@ package com.pingchuan.wufu.apifeign.Controller;
 import com.github.pagehelper.PageInfo;
 import com.pingchuan.Model.User;
 import com.pingchuan.wufu.apifeign.Service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "物流接口")
 @RestController
 public class UserController {
 
@@ -18,6 +21,7 @@ public class UserController {
         return userService.findAllByPage(pageNum, pageSize);
     }
 
+    @ApiOperation(value = "物流接口非第三方接口",notes = "物流接口非第三方接口",httpMethod = "GET")
     @RequestMapping("/verification")
     public User verification(String username, String password){
         return userService.findOneByUsernameAndPassword(username, password);
